bf051290af4c799b9a5ada35372f3840c807c524
unknown <rappoport.marina@gmail.com>
2018-07-13 14:42:52 +0300

    initial commit

---
+++ src/TheadedBinaryTree.java
@@ -0,0 +1,41 @@
+/**
+ * Represents data structure Threaded Binary Tree
+ */
+public class TheadedBinaryTree {
+
+    private Node root;
+
+    public void insert(int key){
+
+    }
+
+    public void remove(int key){
+
+    }
+
+    public Node search(int key){
+        return null;
+    }
+
+    public Node successor(Node node){
+        return null;
+    }
+
+    public Node predecessor(Node node){
+        return null;
+    }
+
+    public Node minimum(){
+        return null;
+    }
+
+    public Node maksimum(){
+        return null;
+    }
+
+    public Node median(){
+        return null;
+    }
+
+
+}
